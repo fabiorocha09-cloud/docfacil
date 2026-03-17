504dda7fcd01386cde8d6466efc166b68fe4df1e
fabio <fabio@scalagestao.com.br>
2026-03-17 00:29:01 +0000

    File changes

--- src/pages/Empresas.jsx
+++ src/pages/Empresas.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { base44 } from "@/api/base44Client";
-import { Plus, Search, Building2, Pencil, Trash2, FileCheck2, SearchCheck, FileSpreadsheet } from "lucide-react";
+import { Plus, Search, Building2, Pencil, Trash2, FileCheck2, SearchCheck, FileSpreadsheet, Mail } from "lucide-react";
 import { Link } from "react-router-dom";
 import EmpresaModal from "@/components/EmpresaModal";
 import SolicitarBuscaModal from "@/components/SolicitarBuscaModal";
